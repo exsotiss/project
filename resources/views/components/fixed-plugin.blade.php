d7ce1725df26bf230b1454437ef71a8becc51965
exsotiss <rosyidmaymunir@gmail.com>
2024-11-15 23:16:17 +0700

    Update fixed-plugin.blade.php

--- resources/views/components/fixed-plugin.blade.php
+++ resources/views/components/fixed-plugin.blade.php
@@ -1,7 +1,7 @@
 <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+    <!-- <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
       <i class="fa fa-cog py-2"> </i>
-    </a>
+    </a> -->
     <div class="card shadow-lg ">
       <div class="card-header pb-0 pt-3 ">
         <div class="{{ (Request::is('rtl') ? 'float-end' : 'float-start') }}">
@@ -10,10 +10,9 @@
         </div>
         <div class="{{ (Request::is('rtl') ? 'float-start mt-4' : 'float-end mt-4') }}">
           <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-            <i class="fa fa-close"></i>
+            <i class="fa fa-close"></i> 
           </button>
         </div>
-        <!-- End Toggle Button -->
       </div>
       <hr class="horizontal dark my-1">
       <div class="card-body pt-sm-3 pt-0">
